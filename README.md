2b277e7604277aab098ab175e259f8a9162fbc7d
Yann Seget <dev@leafgard.fr>
2019-05-22 12:03:04 +0200

    Projet fourni

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Projet-UML-Java
+<h3 align="center">Projet Java</h3>
 
-Projet Semestre 2, Bloc 5, Conception et POO
+<div align="center">
+  <strong>Projet Java - Groupe 10, Conception & POO</strong>
+</div>
+<div align="center">
+ Il s’agit de réécrire un vieux jeu (1984) Boulder Dash crée par Peter Liepa et Chris Gray.
+Le principe du jeu consiste à ramasser un nombre défini de diamants pour ouvrir un passage vers le niveau suivant. Le personnage, surnommé Rockford, doit creuser la terre pour se frayer un chemin. Il devra faire attention à ne pas se faire écraser par un rocher ou un diamant mais également ne pas se faire toucher par les ennemis.
+</div>
--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -1,16 +1,18 @@
-package main;
-
-import java.sql.SQLException;
-
-import controller.ControllerFacade;
-import model.ModelFacade;
-import view.ViewFacade;
-
 /**
- * <h1>The Class Main.</h1>
- *
  * @author Jean-Aymeric DIET jadiet@cesi.fr
  * @version 1.0
+ */
+package main;
+
+import contract.ControllerOrder;
+import controller.Controller;
+import model.Model;
+import view.View;
+
+/**
+ * The Class Main.
+ *
+ * @author Jean-Aymeric Diet
  */
 public abstract class Main {
 
@@ -21,13 +23,12 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+        final Model model = new Model();
+        final View view = new View(model);
+        final Controller controller = new Controller(view, model);
+        view.setController(controller);
 
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
+        controller.control();
+        controller.orderPerform(ControllerOrder.English);
     }
-
 }
